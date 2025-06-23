b70c828d90ede3f81207f1a1d1c62bd1a23ac687
Guarotav <guarotav@gmail.com>
2025-06-23 13:24:46 -0400

    Initial commit

--- src/app.jsx
+++ src/app.jsx
@@ -5,7 +5,39 @@ import "./style.css";
 const App = () => {
   return (
     <div className="app">
-      <h1 className="title">Hello World</h1>
+       <h1>Grid Maker</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+      <div>
+        <button id="add-row">Add Row</button>
+        <button id="delete-row">Delete Row</button> 
+        <button id="add-column">Add Column</button>
+         <button id="delete-column">Delete Column</button>
+        <select id="color-select">
+          <option value="red">Red</option>
+          <option value="blue">Blue</option>
+        </select>
+        <button id="fill-uncolored">Fill Uncolored</button>
+        <button id="fill-grid">Fill Grid</button>
+        <button id="clear-grid">Clear Grid</button>
+      </div>
     </div>
   );
 };
